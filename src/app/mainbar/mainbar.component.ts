bb73e63fce9637c11444a1a7f87cb9e5ac13244c
Stephen Yoon <yoonic@gmail.com>
2020-02-22 11:15:11 -0500

    Initial commit to get things going from angular.io

---
+++ src/app/mainbar/mainbar.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }                 from '@angular/router';
+
+@Component({
+  selector: 'app-mainbar',
+  templateUrl: './mainbar.component.html',
+  styleUrls: ['./mainbar.component.css']
+})
+export class MainbarComponent implements OnInit {
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+  }
+
+  cancel() {
+    this.closePopup();
+  }
+
+  closePopup() {
+    // Providing a `null` value to the named outlet
+    // clears the contents of the named outlet
+    this.router.navigate([{ outlets: { popup: null }}]);
+  }
+}

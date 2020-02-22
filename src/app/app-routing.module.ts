80c521d91e848cb5fd2652e114e737b87700202f
Stephen Yoon <yoonic@gmail.com>
2020-02-22 11:33:07 -0500

    Removed lazy loading module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,15 @@ import { MainbarComponent } from './mainbar/mainbar.component';
 import { AuthGuard }                          from './auth/auth.guard';
 import { SelectivePreloadingStrategyService } from './selective-preloading-strategy.service';
 
+
+import { CanDeactivateGuard }             from '../app/can-deactivate.guard';
+import { CrisisDetailResolverService }    from './crisis-center/crisis-detail-resolver.service';
+import { CrisisCenterHomeComponent } from './crisis-center/crisis-center-home/crisis-center-home.component';
+import { CrisisListComponent }       from './crisis-center/crisis-list/crisis-list.component';
+import { CrisisCenterComponent }     from './crisis-center/crisis-center/crisis-center.component';
+import { CrisisDetailComponent }     from './crisis-center/crisis-detail/crisis-detail.component';
+import { SidebarComponent } from './crisis-center/sidebar/sidebar.component';
+
 const appRoutes: Routes = [
   {
     path: 'compose',
@@ -26,8 +35,39 @@ const appRoutes: Routes = [
   },
   {
     path: 'crisis-center',
-    loadChildren: () => import('./crisis-center/crisis-center.module').then(m => m.CrisisCenterModule),
-    data: { preload: true }
+    component: CrisisCenterComponent,
+    children: [
+      {
+        path: 'sidebar',
+        component: SidebarComponent,
+        outlet: 'popup2'
+      },
+      
+      {
+        path: 'foo',
+        component: SidebarComponent,
+      },
+      {
+        path: '',
+        component: CrisisListComponent,
+        children: [
+          {
+            path: ':id',
+            component: CrisisDetailComponent,
+            canDeactivate: [CanDeactivateGuard],
+            resolve: {
+              crisis: CrisisDetailResolverService
+            }
+          },
+          {
+            path: '',
+            component: CrisisCenterHomeComponent
+          }
+        ]
+      }
+    ]
+    //loadChildren: () => import('./crisis-center/crisis-center.module').then(m => m.CrisisCenterModule),
+    //data: { preload: true }
   },
   { path: '',   redirectTo: '/superheroes', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
